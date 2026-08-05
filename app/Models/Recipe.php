78cad8a1cb5c5187ed19bc36af5cdec48731a1ff
Ian Ewing <ian@ewing.codes>
2026-08-05 10:47:09 -0400

    build image attribute getter

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Services\NutritionCalculationService;
 use App\Services\RecipeScalingService;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Storage;
@@ -32,6 +33,12 @@ class Recipe extends Model
     ];
 
     protected $appends = ['image_url'];
+
+    protected function imageName(): Attribute {
+        return Attribute::make(
+            get: fn (?string $value) => Storage::disk('recipe-images')->url($value),
+        );
+    }
 
     public function getImageUrlAttribute(): ?string
     {
